df13d4c477222b654d01c5c28ec75b0f71f4ee89
Efraim Gentil <efraimgentil@gmail.com>
2014-12-03 22:34:40 -0300

    Arquillian config, and arquillian test example

--- src/test/java/br/com/cinematizando/service/MovieServiceTest.java
+++ src/test/java/br/com/cinematizando/service/MovieServiceTest.java
@@ -1,20 +1,63 @@
 package br.com.cinematizando.service;
 
-import java.util.Properties;
+import java.util.Date;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
+import javax.inject.Inject;
 import javax.naming.NamingException;
-import javax.persistence.Temporal;
 
-import org.apache.openejb.client.LocalInitialContextFactory;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import br.com.cinematizando.dao.AbstractDao;
+import br.com.cinematizando.dao.MovieDAO;
+import br.com.cinematizando.model.Model;
+import br.com.cinematizando.model.Movie;
+import br.com.cinematizando.producer.EntityManagerProducer;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
 public class MovieServiceTest {
+  
+  @Deployment
+  public static Archive<?> prepareDeployment(){
+    return ShrinkWrap.create( JavaArchive.class )
+        .addClass( EntityManagerProducer.class)
+        .addClass( Model.class)
+        .addClass( Movie.class )
+        .addClass( CrudService.class )
+        .addClass( MovieService.class )
+        .addClass( MovieServiceImpl.class )
+        .addClass( MovieDAO.class )
+        .addClass( AbstractDao.class )
+        .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+        .addAsManifestResource(EmptyAsset.INSTANCE , "beans.xml");
+  }
+  
+  @Inject MovieService service;
   
   @Test
   public void teste() throws NamingException{
+    Movie movie = createMovie();
     
+    service.insert( movie );
+    
+    assertNotNull( movie.getId() );
+  }
+  
+  
+  public static Movie createMovie(){
+    Movie movie = new Movie();
+    movie.setOriginalTitle("The Arquillian Atack");
+    movie.setRelease( new Date() );
+    movie.setSynopsis("Thats it, not really easy");
+    return movie;
   }
   
 }
